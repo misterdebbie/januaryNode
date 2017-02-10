90b543f88c910debf117f3084e668c9496d83c51
mr.debbie <giyoungchong1980@gmail.com>
2017-02-10 15:30:32 -0600

    angular test

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -2,6 +2,20 @@
  * Created by debrachong on 1/9/17.
  */
 
-module.exports.homepageController = function(req,res){
+/*module.exports.homepageController = function(req,res){
     res.render('home', {title: 'The Homepage!!!'});
+};*/
+
+var renderTestpage = function(req, res){
+    res.render('locTest', {
+        title: 'Yang Site!',
+        pageHeader: {
+            title: 'Angular Yang Test Site!!!',
+            strapline: 'Must learn Angular and React!'
+        },
+        sidebar: "{{{{haha}}}!!!!!!"
+});
+};
+module.exports.testController = function(req,res){
+    renderTestpage(req,res);
 };
