07f1acf9a3f8ef40d232b41ffc360aa678748bcd
mr.debbie <giyoungchong1980@gmail.com>
2017-06-23 20:38:41 -0500

    yang test

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var router = express.Router();
+//var ctrlHome = require('../controllers/home');
 var ctrlHome = require('../controllers/home');
-var ctrlLogin = require('../controllers/login');
+/*var ctrlLogin = require('../controllers/login');
 var ctrlShop = require('../controllers/shop');
 var ctrlRun = require('../controllers/run');
-var ctrlWish = require('../controllers/wishes');
+var ctrlWish = require('../controllers/wishes');*/
 
 
 /* GET home page */
-router.get('/', ctrlHome.homepageController);
+//router.get('/', ctrlHome.homepageController);
+/* GET home page */
+router.get('/', ctrlHome.testController);
 
 /* GET login page */
-router.get('/login', ctrlLogin.loginController);
+//router.get('/login', ctrlLogin.loginController);
 
 /* GET shop page */
-router.get('/shop', ctrlShop.shopController);
+//router.get('/shop', ctrlShop.shopController);
 
 /* GET runway page */
-router.get('/runway', ctrlRun.runController);
+//router.get('/runway', ctrlRun.runController);
 
 
 /* GET wishlist page */
-router.get('/wish', ctrlWish.wishlistController);
+//router.get('/wish', ctrlWish.wishlistController);
 
 module.exports = router;
