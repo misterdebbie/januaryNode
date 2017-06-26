0ecde7c3f17a494e26c8b35b4fc27bfcbf2ca61c
mr.debbie <giyoungchong1980@gmail.com>
2017-06-25 19:28:38 -0500

    yang test

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./app_api/models/db');
 
 var index = require('./app_server/routes/index');
-var routesApi = require('./app_api/routes/index');
+//var routesApi = require('./app_api/routes/index');
 
 //var users = require('./app_server/routes/users');
 
@@ -20,6 +20,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static('public'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', routesApi);
+//app.use('/api', routesApi);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
